51fd928d91d7f45c476ac90bfe90dd4f87d49395
bing zhao <zhaobingsnowhit@qq.com>
2019-09-04 11:41:54 +0800

    add cc2500 module

---
+++ HARDWARE/cc2500.c
@@ -0,0 +1,41 @@
+#include "cc2500.h"
+#include "spi.h"
+
+uint8_t CC2500_Init(void)
+{
+	return 0;
+}
+
+void CC2500_SetPower(uint8_t power)
+{
+
+}
+
+void CC2500_Strobe(uint8_t state)
+{
+
+
+}
+
+void CC2500_WriteReg(uint8_t address, uint8_t data)
+{
+
+	
+}
+
+uint8_t CC2500_ReadReg(uint8_t address)
+{
+
+	return 0;
+}
+
+void CC2500_SetTxRxMode(uint8_t mode)
+{
+
+}
+
+void CC2500_WriteData(uint8_t *dpbuffer, uint8_t len)
+{
+
+}
+
